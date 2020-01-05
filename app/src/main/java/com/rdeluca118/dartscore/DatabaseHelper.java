1d2e52d41934ada89e3363a51c525cf37d6e3634
deluc <deluca118@gmail.com>
2020-01-05 16:54:46 -0400

    Table creation an deletion work

--- app/src/main/java/com/rdeluca118/dartscore/DatabaseHelper.java
+++ app/src/main/java/com/rdeluca118/dartscore/DatabaseHelper.java
@@ -11,8 +11,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_LEG = "leg";
     public static final String TABLE_TURN = "turn";
 
-
-
     // Game Table columns
     public static final String GAME_ID = "_id";
     public static final String game_date = "date";
@@ -20,17 +18,28 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String game_player2 = "player2_id";
     public static final String game_num_legs = "max_legs";
 
-    // Creating table query
+    // Creating game query
     private static final String CREATE_GAME_TABLE = "create table " + TABLE_GAME + "(" + GAME_ID
             + " INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, " + game_date + " DATETIME NOT NULL, " + game_player1 + " INTEGER NOT NULL, "  + game_player2 + " INTEGER NOT NULL, " + game_num_legs + " INTEGER);";
+
+    private static final String CREATE_GAME_INDEX ="CREATE INDEX 'date_idx' ON '" + TABLE_GAME +"' ('"+ game_date + "' ASC)";
+
     // Player Table columns
 
     public static final String PLAYER_ID = "_id";
     public static final String player_name = "name";
 
+    // creating player query
+    private static final String CREATE_PLAYER_TABLE = "create table " + TABLE_PLAYER + "(" + PLAYER_ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, " + player_name + " VARCHAR(20) NOT NULL);";
+
     // Leg Table columns
     public static final String LEG_ID = "_id";
     public static final String game_id = "game_id";
+
+    // creating leg query
+    private static final String CREATE_LEG_TABLE = "create table " + TABLE_LEG + "(" + LEG_ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, " + game_id + " INTEGER NOT NULL, FOREIGN KEY('game_id') REFERENCES 'game'('id'));";
 
     // Turn Table columns
     public static final String TURN_ID = "_id";
@@ -40,9 +49,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String dart_two = "dart_2";
     public static final String dart_three = "dart_3";
 
+    // creating leg query
+    private static final String CREATE_TURN_TABLE = "create table " + TABLE_TURN + "(" + TURN_ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, " + player_id + " INTEGER NOT NULL, " + leg_id + " INTEGER NOT NULL, " + dart_one + " INTEGER, " + dart_two + " INTEGER, " + dart_three + " INTEGER, FOREIGN KEY('leg_id') REFERENCES 'leg'('id'), FOREIGN KEY('player_id') REFERENCES 'player'('id'));";
+
     // Database Information
     static final String DB_NAME = "darts118";
-
     // database version
     static final int DB_VERSION = 1;
 
@@ -53,11 +65,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db){
         // game table
         db.execSQL(CREATE_GAME_TABLE);
-        db.execSQL("CREATE INDEX 'date_idx' ON 'game' ('date' ASC)");
+        db.execSQL(CREATE_GAME_INDEX);
+        // player table
+        db.execSQL(CREATE_PLAYER_TABLE);
+        // leg table
+        db.execSQL(CREATE_LEG_TABLE);
+        // turn table
+        db.execSQL(CREATE_TURN_TABLE);
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        db.execSQL("DROP INDEX IF EXISTS date_idx");
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_GAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PLAYER);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_LEG);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TURN);
+
         onCreate(db);
     }
 }
